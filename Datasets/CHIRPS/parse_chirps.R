e68e6872f079e8ce2352b7cde0325f3fb6c6e583
Adnan <adfi@users.noreply.github.com>
2016-05-12 22:09:31 +0100

    Update parse_chirps.R

--- Datasets/CHIRPS/parse_chirps.R
+++ Datasets/CHIRPS/parse_chirps.R
@@ -6,6 +6,10 @@ precip <- nc_data$precip # take the 3d precipitation matrix
 # add the names for the dimensions for the next step
 dimnames(precip)<-list(d1=nc_data$longitude, d2=nc_data$latitude, d3=nc_data$time)
 # now when we convert this to a table the dimnames will be automatically set
-df <- as.data.frame.table(precip, responseName = "value")
+df <- as.data.frame.table(precip, responseName = "value", stringsAsFactors = FALSE)
 # there seem to be quite a few missing values
 relevant <- df[!is.na(df$value) & df$value!=0, ]
+colnames(relevant) <- c("longitude", "latitude", "time", "value")
+relevant$latitude <- as.numeric(relevant$latitude)
+relevant$longitude <- as.numeric(relevant$longitude)
+relevant$time <- as.numeric(relevant$time)
